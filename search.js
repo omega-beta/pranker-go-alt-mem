3ba00305c812311474e7fd110988ae2def6ff659
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-10-04 14:45:42 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,7 @@
 var searchup = document.getElementsByName("search");
 var results = document.getElementById("result");
+var display = document.getElementById("display-result");
 if (searchup.length > 0) {
   results.toggle.classList("results-valid");
+  display.innerHTML = searchup.value;
 }
